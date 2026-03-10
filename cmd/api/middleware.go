3022fc6a5ecf6e8720918fa8cc0ddecc47b099f1
Jennessa Sierra <2021153908@ub.edu.bz>
2026-03-09 23:12:37 -0600

    feat: implemented ip-based rate limiting

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
+// recovers from any panics and sends a 500 Internal Server Error response
 func (a *applicationDependencies) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// defer will be called when the stack unwinds
@@ -22,16 +26,59 @@ func (a *applicationDependencies) recoverPanic(next http.Handler) http.Handler {
 	})
 }
 
+// limit the number of requests a client can make in a given time period
 func (a *applicationDependencies) rateLimit(next http.Handler) http.Handler {
-	// create a new rate limiter that allows 2 requests per second with a maximum burst size of 5
-	limiter := rate.NewLimiter(2, 5)
+	// Define a rate limiter struct
+	type client struct {
+		limiter  *rate.Limiter
+		lastSeen time.Time // remove map entries that are stale
+	}
+
+	var mu sync.Mutex                      // use to synchronize the map
+	var clients = make(map[string]*client) // the actual map
+
+	// A goroutine to remove stale entries from the map
+	go func() {
+		for {
+			time.Sleep(time.Minute)
+			mu.Lock() // begin cleanup
+			// delete any entry not seen in three minutes
+			for ip, client := range clients {
+				if time.Since(client.lastSeen) > 3*time.Minute {
+					delete(clients, ip)
+				}
+			}
+			mu.Unlock() // finish clean up
+		}
+	}()
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// check if the request is allowed by the rate limiter
-		// if not, send a 429 Too Many Requests response
-		if !limiter.Allow() {
+		// get the IP address
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			a.serverErrorResponse(w, r, err)
+			return
+		}
+
+		mu.Lock() // exclusive access to the map
+
+		// check if ip address already in map, if not add it
+		_, found := clients[ip]
+		if !found {
+			clients[ip] = &client{limiter: rate.NewLimiter(2, 5)}
+		}
+
+		// Update the last seen for the client
+		clients[ip].lastSeen = time.Now()
+
+		// Check the rate limit status
+		if !clients[ip].limiter.Allow() {
+			mu.Unlock() // no longer need exclusive access to the map
 			a.rateLimitExceededResponse(w, r)
 			return
 		}
+
+		mu.Unlock() // others are free to get exclusive access to the map
 		next.ServeHTTP(w, r)
 	})
 }
